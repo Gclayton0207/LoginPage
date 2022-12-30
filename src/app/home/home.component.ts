738a855cc28883ee46827331c469f84aad432456
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-12-30 19:28:12 -0300

    feat:usando interpolacao

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  constructor(){
+  }
+
+  headername = 'Bem vindo';
+  
+
+  ngOnInit(): void {
+
+  }
+
+  functionclick(name: string) {
+    alert(name)
+
+  }
 }
+
+
