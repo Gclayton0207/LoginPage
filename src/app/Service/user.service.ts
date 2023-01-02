2c8addc745d8e975da82c9ea2d3af56749671749
Giovanni Clayton <giovanni.gcda@gmail.com>
2023-01-01 21:49:12 -0300

    feat: implementando ativacao de rotas

--- src/app/Service/user.service.ts
+++ src/app/Service/user.service.ts
@@ -5,12 +5,20 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) {}
 
-  }
-
-  ProceedLogin(inputdata: any){
+  ProceedLogin(inputdata: any) {
     console.log(inputdata);
     return this.http.post('http://localhost:3000/auth/login', inputdata);
+  }
+
+  IsLooged() {
+    return localStorage.getItem('token') != null;
+  }
+
+  GetToken() {
+    return localStorage.getItem('token') != null
+      ? localStorage.getItem('token')
+      : '';
   }
 }
